123a40896bb09c8168896f4f8d4f02924d7ad285
Ryan <ryankanno@localkinegrinds.com>
2020-03-30 15:03:05 +0900

    Added in datetime into folder structure to separate out different runs

--- scripts/hawaii/run.sh
+++ scripts/hawaii/run.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-OUTPUT_ROOT="${DIR}/../../results/hawaii/"
-
-# create if not exists
-mkdir -p "${OUTPUT_ROOT}"
+OUTPUT_ROOT="${DIR}/../../results/hawaii"
 
 DATE_PRE=`date +%Y%m%d_%H%M%S`
 
+SESSION_OUTPUT_PATH="${DIR}/../../results/hawaii/${DATE_PRE}"
+
+# create if not exists
+mkdir -p "${SESSION_OUTPUT_PATH}"
+
 CSV_FILENAME="${DATE_PRE}_hawaii_daily.csv"
-CSV_PATH="${OUTPUT_ROOT}${CSV_FILENAME}"
+CSV_PATH="${SESSION_OUTPUT_PATH}/${CSV_FILENAME}"
 
 SQLITE_FILENAME="${DATE_PRE}_hawaii_daily.sqlite.db"
-SQLITE_PATH="${OUTPUT_ROOT}${SQLITE_FILENAME}"
+SQLITE_PATH="${SESSION_OUTPUT_PATH}/${SQLITE_FILENAME}"
 
 ${DIR}/download.sh ${CSV_PATH}
 ${DIR}/create_sqlite.sh ${CSV_PATH} ${SQLITE_PATH}
