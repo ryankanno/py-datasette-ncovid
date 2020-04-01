01108f8bd3d3bb83bb80b0f8501f065cd3fdb561
Ryan <ryankanno@localkinegrinds.com>
2020-04-01 17:33:10 +0900

    Updated deploy.sh text

--- scripts/hawaii/deploy.sh
+++ scripts/hawaii/deploy.sh
@@ -29,6 +29,6 @@ cd ${SESSION_OUTPUT_PATH}
 HUMAN_FORMAT="+%B %d, %Y @ %T %Z"
 HUMAN_LAST_PUBLISHED=`gdate "${HUMAN_FORMAT}"`
 
-datasette publish heroku --name covid19-hawaii --title "COVID-19 Hawaii Daily Test Results (Last Published: ${HUMAN_LAST_PUBLISHED})" --version-note \""Data Imported: ${DATE_PRE}"\" --source "Community Maintained Daily Hawaii COVID-19 Metrics" --source_url "https://docs.google.com/spreadsheets/d/1WTUg75_mm35Z2C6PQd5AOKtVeSNfci5Mp0G_pgYTqQI/edit#gid=0" --license "Creative Commons Attribution 4.0 International" --license_url "https://creativecommons.org/licenses/by/4.0/" --about "This is a community maintained, unofficial table of COVID-19 stats compiled from DOH and media reports. Accuracy is not guaranteed. Please see the Data Source link to report any errors." "${DATASETTE_FILENAME}"
+datasette publish heroku --name covid19-hawaii --title "COVID-19 Hawaii Daily Test Results (Last Published: ${HUMAN_LAST_PUBLISHED})" --version-note \""Data Imported: ${DATE_PRE}"\" --source "Community Maintained Daily Hawaii COVID-19 Metrics" --source_url "https://docs.google.com/spreadsheets/d/1WTUg75_mm35Z2C6PQd5AOKtVeSNfci5Mp0G_pgYTqQI/edit#gid=0" --license "Creative Commons Attribution 4.0 International" --license_url "https://creativecommons.org/licenses/by/4.0/" --about "This is a community maintained, unofficial table of COVID-19 stats compiled from DOH and media reports. Accuracy is not guaranteed. Please see the Data source link to report any errors." "${DATASETTE_FILENAME}"
 
 echo "Data Imported: ${DATE_PRE}, Last Published: ${HUMAN_LAST_PUBLISHED}"
